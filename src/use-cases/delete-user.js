58056e7b1eccc62900e535fcf62bf0a1270ce2eb
Samuel-3004 <samuelszportela@hotmail.com>
2026-01-22 10:22:14 -0300

    feat: add DeleteUserUseCase

---
+++ src/use-cases/delete-user.js
@@ -0,0 +1,11 @@
+import { PostgresDeleteUserRepository } from '../repositories/postgres/delete-user.js'
+
+export class DeleteUserUseCase {
+    async execute(userId) {
+        const postgresDeleteUserRepository = new PostgresDeleteUserRepository()
+
+        const deleteUser = await postgresDeleteUserRepository.execute(userId)
+
+        return deleteUser
+    }
+}
