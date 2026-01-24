07eb0b2d24a06627ac10d40083e2041738a4b19d
Samuel-3004 <samuelszportela@hotmail.com>
2026-01-24 10:41:17 -0300

    feat: add CreateTransactionController

---
+++ src/controllers/transaction/create-transaction.js
@@ -0,0 +1,80 @@
+import validator from 'validator'
+import {
+    badRequest,
+    checkIfIdIsValid,
+    created,
+    invalidIdResponse,
+    serverError,
+} from '../helpers'
+
+export class CreateTransactionController {
+    constructor(createTransactionUseCase) {
+        this.createTransactionUseCase = createTransactionUseCase
+    }
+
+    async execute(httpRequest) {
+        try {
+            const params = httpRequest.body
+
+            const requiredFields = ['user_id', 'name', 'date', 'amount', 'type']
+
+            for (const field of requiredFields) {
+                if (!params[field] || params[field].trim().length === 0) {
+                    return badRequest({
+                        message: `Missing param: ${field}`,
+                    })
+                }
+            }
+
+            const userIdISValid = checkIfIdIsValid(params.user_id)
+
+            if (!userIdISValid) {
+                return invalidIdResponse()
+            }
+
+            if (params.amount <= 0) {
+                return badRequest({
+                    message: 'The amount must be greater than zero.',
+                })
+            }
+
+            const amountIsValid = validator.isCurrency(
+                params.amount.toString(),
+                {
+                    digits_after_decimal: [2],
+                    allow_negatives: false,
+                    decimal_separator: '.',
+                },
+            )
+
+            if (!amountIsValid) {
+                return badRequest({
+                    message: 'The amount must be a valid currency value.',
+                })
+            }
+
+            const type = params.type.trim().toUpperCase()
+
+            const typeIsValid = ['EARNING', 'EXPENSE', 'INVESTMENT'].includes(
+                type,
+            )
+
+            if (!typeIsValid) {
+                return badRequest({
+                    message:
+                        'The type must be one of the following: EARNING, EXPENSE, INVESTMENT.',
+                })
+            }
+
+            const transaction = await this.createTransactionUseCase.execute({
+                ...params,
+                type,
+            })
+
+            return created(transaction)
+        } catch (error) {
+            console.log(error)
+            return serverError()
+        }
+    }
+}
