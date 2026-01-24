81e92790e8d839f51b388a59fc4b7666093d8ad2
Samuel-3004 <samuelszportela@hotmail.com>
2026-01-24 10:01:43 -0300

    refactor: move user controller to its own folder

--- src/controllers/user/delete-user.js
+++ src/controllers/user/delete-user.js
@@ -4,7 +4,7 @@ import {
     serverError,
     success,
     userNotFoundResponse,
-} from './helpers/index.js'
+} from '../helpers/index.js'
 
 export class DeleteUserController {
     constructor(deleteUserUseCase) {
