63af31bb12dd72bf00267c61797e3e6c3da52bce
Samuel-3004 <samuelszportela@hotmail.com>
2026-02-07 10:17:09 -0300

    feat: ensure PostgresGetUserByIdRepository use Prisma

--- src/repositories/postgres/user/get-user-by-id.js
+++ src/repositories/postgres/user/get-user-by-id.js
@@ -2,7 +2,7 @@ import { prisma } from '../../../../prisma/prisma.js'
 
 export class PostgresGetUserByIdRepository {
     async execute(userId) {
-        return prisma.user.findUnique({
+        return await prisma.user.findUnique({
             where: {
                 id: userId,
             },
